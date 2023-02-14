6abe2e7bec9357b1d8d6e9913aee3dce5bd1706d
parth <mittalparth22@gmail.com>
2023-02-15 02:07:14 +0530

    landing: Add Landing Page.

--- greentrust/src/components/Button.js
+++ greentrust/src/components/Button.js
@@ -1,13 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Button({ text, icon, color }) {
+export default function Button({ text, icon, color, styles }) {
   return (
     <div>
       <a
         role="button"
         class={
           color +
-          " hover:bg-red-700 text-white text-sm px-4 py-2 items-center border rounded-full text-lg md:text-lg font-comfortaa mr-2"
+          ` hover:bg-red-700 text-white text-sm px-4 py-2 items-center border rounded-full text-lg md:text-lg font-comfortaa mr-2 ${styles ?? ''}`
         }
       >
         <FontAwesomeIcon

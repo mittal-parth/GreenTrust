e5c6c791fadeaf930ff4e066c0399490f2a3cc16
Mehul Todi <75692643+kid-116@users.noreply.github.com>
2023-02-16 01:16:29 +0530

    Add contexts for snackbar and loader

--- greentrust/src/components/Spinner.js
+++ greentrust/src/components/Spinner.js
@@ -3,7 +3,7 @@ import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Spinner() {
     return (
-        <div className="fixed top-0 w-full h-full bg-darkGray/30 flex items-center justify-center">
+        <div className="fixed top-0 left-0 w-screen h-screen bg-darkGray/30 flex items-center justify-center">
             <CircularProgress />
         </div>
     );

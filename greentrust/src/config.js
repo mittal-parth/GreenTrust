c6817e9efe0c3c0d5905f471f12fc8da52cbade2
pranav2305 <pranav2305@gmail.com>
2023-02-16 04:43:37 +0530

    Add fetchFarmDetails function in contract

--- greentrust/src/config.js
+++ greentrust/src/config.js
@@ -1,2 +1,2 @@
 export const appAddress = "851e2f9b3e599f9ea4f8f1a3c2c9012d3543dc2b";
-export const contractAddress = "0xce227034Ba66E0BdD4e81AEE51bA891b65433d02";
+export const contractAddress = "0x173616AEAAF77AD6a719cd4dE9A196ebFf1C476b";

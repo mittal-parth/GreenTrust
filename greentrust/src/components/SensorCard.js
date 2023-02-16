388188d718dab417cdcc58633b84744aff689ab2
Mehul Todi <75692643+kid-116@users.noreply.github.com>
2023-02-16 21:10:39 +0530

    Render sensor and stakeholder info for crop

--- greentrust/src/components/SensorCard.js
+++ greentrust/src/components/SensorCard.js
@@ -1,20 +1,21 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHashtag, faChartLine } from "@fortawesome/free-solid-svg-icons";
 
-export default function SensorCard({ sensorDetails }) {
+export default function SensorCard({ details }) {
+
   return (
     <div className="rounded-[20px]  shadow-xl flex justify-center items-center w-fit mx-3 px-3">
       <div className=" my-6 space-y-3 ">
         <div className="flex flex-row items-center">
         <FontAwesomeIcon icon={faChartLine}  style={{ color: "black" }}/>
           <p className="font-comfortaa font-bold text-xl text-darkGray ml-3">
-            {sensorDetails.get("sensorName")}
+            {details.name}
           </p>
         </div>
         <div className="flex flex-row">
           <FontAwesomeIcon icon={faHashtag} style={{ color: "black" }}/>
           <p className="font-comfortaa font-bold  text-sm text-darkGray ml-3">
-            {sensorDetails.get("sensorId")}
+            {parseInt(details.id._hex)}
           </p>
         </div>
       </div>

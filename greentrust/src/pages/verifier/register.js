2cd9f13818ecc55dfb07eb4b974b943a46464b03
shashank <shash.201cs257@gmail.com>
2023-02-17 14:34:09 +0530

    Integrate of register v\erifier feature

--- greentrust/src/pages/verifier/register.js
+++ greentrust/src/pages/verifier/register.js
@@ -4,7 +4,7 @@ import { useEffect, useState, useContext } from "react";
 import { useAuth } from "@arcana/auth-react";
 import { LoaderContext } from "@/context/loaderContext";
 import { SnackbarContext } from "@/context/snackbarContext";
-import { contractCall } from "@/utils";
+import { contractCall, uploadFile } from "@/utils";
 
 export default function Register() {
   const { loading, setLoading } = useContext(LoaderContext);
@@ -14,44 +14,53 @@ export default function Register() {
 
   const auth = useAuth();
   const [verifierProfile, setVerifierProfile] = useState({});
-  const [idCards, setIdCards] = useState("Hard Coded");
+  const [ids, setIds] = useState([]);
   const { snackbarInfo, setSnackbarInfo } = useContext(SnackbarContext);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("Handling Inputs")
-
+    const fileHashes = await uploadFile(ids.length == 1 ? [ids] : Object.values(ids) );
+    console.log(fileHashes, "THESE ARE THE FILE HASHES")
+    var idCardsHash =""
+    fileHashes.forEach((fH) =>{
+      idCardsHash += fH[0].hash + " "
+    });
+    console.log(idCardsHash)
+    console.log(auth.user)
     if (auth.user) {
-      postVerifierInfo();
+      postVerifierInfo(idCardsHash);
     }
 
     };
 
-  const postVerifierInfo = async () => {
+  const postVerifierInfo = async (idCardsHash) => {
 
     setLoading(true);
-    console.log("Registering verifier");
+
     console.log(auth.user)  
-    const res = await contractCall(auth, "registerVerifier", [
-      verifierProfile.name,
-      verifierProfile.currentAddress,
-      idCards,
-    ]);
-    if (res.status == 200) {
-      console.log(res.data, "Response");
+
+    try{
+      const res = await contractCall(auth, "registerVerifier", [
+        verifierProfile.name,
+        verifierProfile.currentAddress,
+        idCardsHash,
+      ]);
       setSnackbarInfo({
         ...snackbarInfo,
         open: true,
         message: `Registered Successfully`,
       });
       router.replace('/dashboard');
-    } else {
-      console.log(res);
+
+    }catch(err){
+      
+      console.log(err)
       setSnackbarInfo({
         ...snackbarInfo,
         open: true,
-        message: `Error ${res.status}: Registration failed`,
-      });
+        message: `Registration failed`,
+        });
+      
     }
     setLoading(false);
   };
@@ -80,9 +89,25 @@ export default function Register() {
             placeHolder={"Address"}
             type={"text"}
           />
+          <div className="mb-10">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold  mb-1"
+              htmlFor="grid-first-name"
+              >
+              Id Card
+            </label>
+            <div>
+              <input
+                type="file"
+                multiple
+                onChange={e=>{setIds(e.target.files);}}
+                className="block w-fit bg-transparent text-gray-700 border border-darkGray rounded-xl rounded py-1 px-4 mb-2 leading-tight focus:bg-white"
+              />
+            </div>
+          </div>
           <div>
             <button
-              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-3"
+              className="bg-red-500 hover:bg-red-700 text-white bg-black font-bold py-2 px-4 rounded mb-3"
               type="submit"
             >
               Submit

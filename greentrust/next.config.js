d12e06447f39d3ff930d2535f8a11606d41cbe6f
Mehul Todi <75692643+kid-116@users.noreply.github.com>
2023-02-20 03:55:30 +0530

    Fix workflow

--- greentrust/next.config.js
+++ greentrust/next.config.js
@@ -1,7 +1,7 @@
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
-  disable: process.env.NODE_ENV === 'development',
+  // disable: process.env.NODE_ENV === 'development',
   skipWaiting: true,
 });
 
@@ -12,9 +12,6 @@ const nextConfig = withPWA({
       fs: false,
     };
     return config;
-  },
-  images: {
-    unoptimized: true,
   },
   reactStrictMode: true,
 });
